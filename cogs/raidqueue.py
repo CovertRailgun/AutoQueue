2dfaf128a899aea53fa48db57af74a149b3b5a45
Jan <73044370+Z1R343L@users.noreply.github.com>
2023-02-03 16:13:33 +0100

    there we go

--- cogs/raidqueue.py
+++ cogs/raidqueue.py
@@ -1,89 +1,148 @@
 from asyncio import Queue, create_task
+from io import BytesIO
 from logging import getLogger
-import json
 
 import disnake
+from aiohttp import ClientSession
 from disnake.ext import commands, components
 
-with open("config.json") as file:
-    config_data = json.load(file)
-servers = config_data["servers"]
 
 class RaidQueue(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        #self.bot._test_guilds(servers)
         self.raid_queue = Queue()
         self.log = getLogger("rich")
         self.raidtask_running = False
         self.queue_running = False
-        self.raid_information = []
+        self.raid_information = None
+        self.types = None
+        self.mons = None
 
-    async def autoraid_task(
-        self,
-        inter
-    ):
+    async def fetch_data(self):
+        self.log.info("fetching assets infos")
+        async with ClientSession() as session:
+            async with session.get(
+                "https://raw.githubusercontent.com/Z1R343L/raid_assets/main/sprites_list.json"
+            ) as response:
+                self.mons = disnake.utils._from_json(await response.text())
+            async with session.get(
+                "https://raw.githubusercontent.com/Z1R343L/raid_assets/main/tera_types.json"
+            ) as response:
+                self.types = disnake.utils._from_json(await response.text())
+
+    async def autoraid_task(self, inter):
         raid_counter = 0
-        #quit game
+        # quit game
         await self.bot.sbbcon.quit_game()
         self.log.info("quit game")
 
         while self.raidtask_running:
-            #start game
+            # start game
             await self.bot.sbbcon.start_game()
             self.log.info("started game")
 
-            #going online
+            # going online
             self.log.info("raidtask started")
-            await self.bot.sbbcon.going_online()
-            self.log.info("connected to internet")
+            if not self.bot.config["offline_mode"]:
+                await self.bot.sbbcon.going_online()
+                self.log.info("connected to internet")
 
-            #raid lobby
+            await self.bot.sbbcon.send_seq("clickSeq W2,A,W1,A,W1,B,W1,B,W1")
+            # raid lobby
             await self.bot.sbbcon.raid_lobby(self.raid_queue)
-            #await self.inter.send("Raid starting")
+            # await self.inter.send("Raid starting")
             self.log.info("starting raid")
 
-            #raid battle
+            # raid battle
             await self.bot.sbbcon.raid_battle()
             self.log.info("raid finished")
 
-            #quit game
+            # quit game
             await self.bot.sbbcon.quit_game()
             self.log.info("quit game")
 
             raid_counter += 1
             self.log.info(f"Raids finished: {raid_counter}")
-            if (self.raid_queue.empty() and not self.queue_running):
+            if self.raid_queue.empty() and not self.queue_running:
                 self.raidtask_running = False
 
         inter.send(f"raid finished, {raid_counter} raids done")
-            
+
     @commands.slash_command(
         name="open_queue",
         description="Starts hosting the raid and opens the queue for users to join.",
-        guild_ids=servers
-        )
+    )
     async def open_queue(
-        self, 
-        inter,
-        raid_level:str,
-        pokemon:str,
-        tera_type:str,
-        notes:str
-        ) -> None:
-
+        self,
+        inter: disnake.AppCommandInter,
+        pokemon: str,
+        tera_type: str,
+        raid_level: int = commands.Param(choices=list(range(1, 7))),
+    ) -> None:
         if inter.author.id != self.bot.owner_id:
-            return None
-        await inter.send(
-            content="Raid hosting has been started!!!"
+            await inter.send("not allowed")
+        await inter.response.send_modal(
+            title="Additional raid info",
+            custom_id="notes_modal",
+            components=[
+                disnake.ui.TextInput(
+                    label="notes",
+                    placeholder="...",
+                    custom_id="notes",
+                    style=disnake.TextInputStyle.multi_line,
+                    min_length=0,
+                    max_length=1024,
+                ),
+            ],
         )
-        self.raid_information = [raid_level, pokemon, tera_type, notes]
+        modal_inter = await self.bot.wait_for(
+            "modal_submit",
+            check=lambda i: i.custom_id == "notes_modal"
+            and i.author.id == inter.author.id,
+        )
+        await modal_inter.response.send_message("done")
+        notes = modal_inter.text_values["notes"]
+
+        emb = disnake.Embed(
+            title=self.bot.config["emojis"]["star"] * raid_level,
+            description=notes,
+            color=disnake.Colour.from_rgb(*self.types[tera_type]),
+        )
+        emb.set_author(
+            name=pokemon.replace("_", " ").replace("female", "").title(),
+            icon_url=f"https://raw.githubusercontent.com/Z1R343L/raid_assets/main/tera_types/{tera_type}.png",
+        )
+        emb.set_thumbnail(
+            f"https://github.com/Z1R343L/raid_assets/raw/main/sprites/shiny/{pokemon}.gif"
+        )
+        self.raid_information = emb
         self.raidtask_running = True
         self.queue_running = True
-        await create_task(self.autoraid_task(inter))
+        create_task(self.autoraid_task(inter))
+
+    @open_queue.autocomplete("pokemon")
+    async def autocomp_mon(self, _: disnake.CommandInteraction, string: str):
+        self.log.info("autocomp mon")
+        if self.mons is None:
+            await self.fetch_data()
+        return (
+            [mon for mon in self.mons if mon.lower().startswith(string.lower())][:25]
+            if string
+            else []
+        )
+
+    @open_queue.autocomplete("tera_type")
+    async def autocomp_tera(self, _: disnake.CommandInteraction, string: str):
+        self.log.info("autocomp tera")
+        string = string.lower()
+        if self.types is None:
+            await self.fetch_data()
+        return [t for t in list(self.types.keys()) if t.lower().startswith(string)]
 
     @components.button_listener()
-    async def join_listener(self, inter: disnake.MessageInteraction, *, author: disnake.User):
+    async def join_listener(
+        self, inter: disnake.MessageInteraction, *, author: disnake.User
+    ):
         self.log.info(author)
         user = inter.author.id
         if user in list(self.raid_queue._queue):
@@ -95,44 +154,39 @@ class RaidQueue(commands.Cog):
     @commands.slash_command(
         name="join_queue",
         description="Provieds information on the current raid and gives you the option to join.",
-        guild_ids=servers
     )
     async def join_queue(self, inter):
-        if self.queue_running == False:
-            await inter.response.defer("queue is closed", ephemeral=True)
+        if not self.queue_running:
+            await inter.send("queue is closed", ephemeral=True)
             return None
         await inter.send(
-            content=f"A {self.raid_information[0]} star tera type {self.raid_information[2]} {self.raid_information[1]} is being hosted.\n{self.raid_information[3]}. \nJoin?",
+            embeds=[self.raid_information],
             components=disnake.ui.Button(
                 label="Join",
-                custom_id=await self.join_listener.build_custom_id(author=inter.author)
+                custom_id=await self.join_listener.build_custom_id(author=inter.author),
             ),
-            ephemeral=True
-            
+            ephemeral=True,
         )
 
     @commands.slash_command(
         name="show_queue",
         description="Shows the users in the queue and where users are.",
-        guild_ids=servers
     )
     async def show_queue(self, inter) -> None:
         if self.raid_queue.empty():
-            await inter.send(
-                content="Queue is empty",
-                ephemeral=True
-            )
+            await inter.send(content="Queue is empty", ephemeral=True)
             return
         await inter.send(
-            content="\n".join([f"<@{str(user)}>" for user in list(self.raid_queue._queue)]),
-            ephemeral=True
+            content="\n".join(
+                [f"<@{str(user)}>" for user in list(self.raid_queue._queue)]
+            ),
+            ephemeral=True,
         )
-    
+
     @commands.slash_command(
         name="close_queue",
         description="Hoster stops people from joining the queue, queue can then empty out before the raid is closed.",
-        guild_ids=servers
-        )  
+    )
     async def close_queue(self, inter) -> None:
         if inter.author.id != self.bot.owner_id:
             return None
@@ -142,13 +196,13 @@ class RaidQueue(commands.Cog):
     @commands.slash_command(
         name="end_raid",
         description="Hoster end hosting the raid imidiatly after the current raid.",
-        guild_ids=servers
-        )
+    )
     async def end_raid(self, inter) -> None:
         if inter.author.id != self.bot.owner_id:
             return None
         self.raidtask_running = False
-        inter.send("Raid hosting will end after the current raid.")
+        await inter.send("Raid hosting will end after the current raid.")
+
 
 def setup(bot: commands.Bot):
     bot.add_cog(RaidQueue(bot))
--- main.py
+++ main.py
@@ -1,49 +1,46 @@
 from __future__ import annotations
 
-from os import environ
-from logging import basicConfig, getLogger
+import json
 from asyncio import Queue, sleep
 from binascii import unhexlify
-from io import BytesIO
-from re import compile
-import json
 from contextlib import suppress
+from io import BytesIO
+from logging import basicConfig, getLogger
+from os import environ
+from re import compile
 
-from aiosbb import SBBClient
 import disnake
-from dotenv import load_dotenv
+from aiosbb import SBBClient
 from disnake.ext import commands
+from dotenv import load_dotenv
 from rich.logging import RichHandler
 
 with suppress(ImportError):
     from uvloop import install
+
     install()
 
 
 def main():
-    basicConfig(
-        level="INFO",
-        format="%(message)s",
-        datefmt="[X]",
-        handlers=[RichHandler()]
-    )
-
     with open("config.json") as file:
         config_data = json.load(file)
-    servers = config_data["servers"]
     ip_address = config_data["IP"]
-
+    basicConfig(
+        level=config_data["log_level"],
+        format="%(message)s",
+        datefmt="[X]",
+        handlers=[RichHandler()],
+    )
     log = getLogger("rich")
     load_dotenv()
 
-    wait_pattern = compile(r"^W{1}(\d*)$")
+    wait_pattern = compile(r"^W(\d*)$")
 
     class SbbConnection(SBBClient):
         def __init__(self, ip):
-            timeout: float = 10.0
-            self.overworldPointer = ["0x43A7848", "0x348", "0x10", "0xD8", "0x28"]
-            self.isConnectedPointer = ["0x437E280", "0x30"]
-            super().__init__(ip)
+            self.overworldPointer = ("0x43A7848", "0x348", "0x10", "0xD8", "0x28")
+            self.isConnectedPointer = ("0x437E280", "0x30")
+            super().__init__(ip, timeout=10.0, verbose=True)
 
         async def send_seq(self, command: str) -> None:
             _, args_str = command.split(" ")
@@ -82,34 +79,41 @@ def main():
             await self.send_seq("clickSeq A,W1,A,W1,A,W1,A,W1")
             await sleep(16)
             await self.send_seq("clickSwq A,W1,A,W1")
+            starting = True
+            while starting:
+                starting = not await self.is_in_overworld()
+                await sleep(1)
 
         async def going_online(self) -> None:
             online = False
             loop_counter = 0
-            while online is not True:
+            while not online:
                 loop_counter += 1
                 await sleep(1)
                 online = await self.is_in_overworld()
-                log.info(f"To see if self.is_in_overworld() is not true attempts: {loop_counter}")
+                log.info(
+                    f"To see if self.is_in_overworld() is not true attempts: {loop_counter}"
+                )
             await self.send_seq("clickSeq X,W1,L")
 
             connected = False
             loop_counter2 = 0
-            while connected is not True:
+            while not connected:
                 loop_counter2 += 1
                 await sleep(2)
                 connected = await self.is_connected()
-                log.info(f"To see if self.is_connected() is not true attempts: {loop_counter2}")
-            await self.send_seq("clickSeq W2,A,W1,A,W1,B,W1,B,W1")
+                log.info(
+                    f"To see if self.is_connected() is not true attempts: {loop_counter2}"
+                )
 
         async def raid_lobby(self, raid_queue: Queue) -> None:
             await self.send_seq("clickSeq A,W3,A,W3,A,W6")
-            file = await self.pixelpeek()
+            _file = await self.pixelpeek()
             users = []
             while not raid_queue.empty() and len(users) < 3:
                 user = await bot.getch_user(await raid_queue.get())
                 users.append(user)
-                await user.send(files=[disnake.File(BytesIO(file), "raid.jpg")])
+                await user.send(files=[disnake.File(BytesIO(_file), "raid.jpg")])
             log.info("code sent")
             await sleep(150)
             await self.send_seq("click A,W3,A")
@@ -119,28 +123,31 @@ def main():
             loop_counter = 0
             while inRaid:
                 loop_counter += 1
-                await self.send_seq("clickSeq A,W1,A,W1,A,W1,A,W1,A,W1,A,W1,B,W1,B,W1,B,W1,B,W1,B,W1,B,W1")
+                await self.send_seq(
+                    "clickSeq A,W1,A,W1,A,W1,A,W1,A,W1,A,W1,B,W1,B,W1,B,W1,B,W1,B,W1,B,W1"
+                )
                 inRaid = not await self.is_in_overworld()
                 await sleep(5)
                 log.info(f"Times though raid: {loop_counter}")
 
     class Bot(commands.InteractionBot):
         def __init__(self, *args, **kwargs) -> None:
-            self.sbbcon = SbbConnection(ip=ip_address)
+            self.config = config_data
+            self.sbbcon = SbbConnection(ip=config_data["IP"])
             super().__init__(*args, **kwargs)
 
-    bot = Bot(
-        intents=disnake.Intents.all(),
-        asyncio_debug=True
-    )
+    bot = Bot(intents=disnake.Intents.all(), asyncio_debug=True)
     bot.load_extensions("cogs")
 
-    @bot.slash_command(guild_ids=servers)
+    @bot.slash_command()
     async def pixelpeek(inter) -> None:
+        await inter.response.defer()
         if inter.author.id != bot.owner_id:
             return None
-        image_hold = await bot.sbbcon("pixelPeek")
-        await inter.send(file=disnake.File(BytesIO(unhexlify(image_hold)), "peek.jpg"), ephemeral=True)
+        image_hold = await bot.sbbcon.pixelpeek()
+        await inter.send(
+            file=disnake.File(BytesIO(image_hold), "peek.jpg"), ephemeral=True
+        )
 
     bot.run(environ["DISCORD_TOKEN"])
 
